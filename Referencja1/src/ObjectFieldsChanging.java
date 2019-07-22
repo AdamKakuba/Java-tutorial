c14a638ea02cb57acfa34ac1f7dc47b1e1be59ce
AdamKakuba <53052352+AdamKakuba@users.noreply.github.com>
2019-07-22 16:47:19 +0200

    Add files via upload

---
+++ Referencja1/src/ObjectFieldsChanging.java
@@ -0,0 +1,29 @@
+public class ObjectFieldsChanging {
+    private int attribute;
+
+    public ObjectFieldsChanging(int attribute) {
+        this.attribute = attribute;
+    }
+
+    public int getAttribute() {
+        return attribute;
+    }
+
+    public void setAttribute(int attribute) {
+        this.attribute = attribute;
+    }
+
+    public static void main(String[] args) {
+        ObjectFieldsChanging reference1 = new ObjectFieldsChanging(123);
+        ObjectFieldsChanging reference2 = reference1;
+        reference2 = new ObjectFieldsChanging(3);
+
+        System.out.println(reference1.getAttribute());
+        System.out.println(reference2.getAttribute());
+
+        reference1.setAttribute(1);
+
+        System.out.println(reference1.getAttribute());
+        System.out.println(reference2.getAttribute());
+    }
+}
