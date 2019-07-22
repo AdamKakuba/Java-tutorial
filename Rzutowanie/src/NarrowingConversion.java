8f3c9e939e5e652cb8b18f1ffc47ee111cc85ad4
AdamKakuba <53052352+AdamKakuba@users.noreply.github.com>
2019-07-22 16:47:57 +0200

    Add files via upload

---
+++ Rzutowanie/src/NarrowingConversion.java
@@ -0,0 +1,10 @@
+public class NarrowingConversion {
+    public static void main(String[] args) {
+        String y=1+"123";
+                System.out.println(y);
+    }
+
+    public void methodIntArgument(int argument) {
+        System.out.println(argument);
+    }
+}
